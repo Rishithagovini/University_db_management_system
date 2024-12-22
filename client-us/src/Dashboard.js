ed9cf83680c49e351d6994c3055fc7966d3a8f0f
RISHITHA GOVINI <52620579+Rishithagovini@users.noreply.github.com>
2024-12-22 13:21:23 -0600

    Update Dashboard.js

--- client-us/src/Dashboard.js
+++ client-us/src/Dashboard.js
@@ -1,5 +1,3 @@
-// Developer: Anjana Priya Bachina and Nikhitha Dadi
-//Last Modified: 29-11-2024
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
